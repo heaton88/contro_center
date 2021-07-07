02bae06a2df4fa7804e122a3bf96812483ab7a5e
heaton88 <5775505@qq.com>
2021-07-07 10:03:23 +0800

    add control file

---
+++ control_center1/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# _author_: heaton
+# _date_: 2021/7/7 9:38 上午
+# _FileName_: __init__.py.py
---
+++ control_center1/control1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# _author_: heaton
+# _date_: 2021/7/7 9:41 上午
+# _FileName_: control1.py
+
+
+import requests
+
