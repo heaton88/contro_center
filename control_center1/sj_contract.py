e2a79379df5c5d03d192e7ae12c6befd11ace0f7
heaton88 <5775505@qq.com>
2021-07-29 15:26:27 +0800

    add api auto test files

--- control_center1/sj_contract.py
+++ control_center1/sj_contract.py
@@ -31,22 +31,20 @@ month_list = []
 for j in range(0,len(code_list)):
     sign_url = 'http://k8sdev.golowo.com/g3-screen-web/manageCenter/queryContractAmount?orgCode='+org_list[j]+','+code_list[j]+'&customerCode='
     year_status = requests.get(sign_url).status_code
+
     if year_status != 200:
         print(name_list[j],"接口报错")
     else:
-        yearamount = json.loads(requests.get(sign_url).content)['data']['yearAmount']
-        monthamount = json.loads(requests.get(sign_url).content)['data']['monthAmount']
-        print(name_list[j],yearamount)
-        year_list.append(yearamount)
-        month_list.append(monthamount)
+        data_status = json.loads(requests.get(sign_url).content)['data']['monthAmountDataVoList']
+        if data_status != []:
+            yearamount = json.loads(requests.get(sign_url).content)['data']['yearAmount']
+            monthamount = json.loads(requests.get(sign_url).content)['data']['monthAmount']
+            print(name_list[j],yearamount)
+            year_list.append(yearamount)
+            month_list.append(monthamount)
+        else:
+            print(name_list[j],code_list[j],"data为空")
 total_company = sum(year_list)
 print(total_company)
 
-
-sj_url_total = 'http://k8sdev.golowo.com/g3-screen-web/manageCenter/queryContractAmount?orgCode=-6944734322880132047,0100000000&customerCode='
-sj_response_year = json.loads(requests.get(sj_url_total).content)['data']['yearAmount']
-sj_response_month = json.loads(requests.get(sj_url_total).content)['data']['monthAmount']
-print(sj_response_year)
-print(sum(month_list))
-print(sj_response_month)
 
